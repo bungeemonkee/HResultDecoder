ea5bbc70c0313e7cdc6c8d3e0fec99c67125890b
David Love <davidl@mediaradar.com>
2015-10-16 16:39:25 -0400

    Existing project

---
+++ HResultDecoder/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace HResultDecoder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.decodeButton = new System.Windows.Forms.Button();
+            this.decodedValue = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.hresultInput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // decodeButton
+            // 
+            this.decodeButton.Location = new System.Drawing.Point(12, 274);
+            this.decodeButton.Name = "decodeButton";
+            this.decodeButton.Size = new System.Drawing.Size(260, 40);
+            this.decodeButton.TabIndex = 0;
+            this.decodeButton.Text = "Decode It!";
+            this.decodeButton.UseVisualStyleBackColor = true;
+            this.decodeButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // decodedValue
+            // 
+            this.decodedValue.Location = new System.Drawing.Point(12, 81);
+            this.decodedValue.Multiline = true;
+            this.decodedValue.Name = "decodedValue";
+            this.decodedValue.ReadOnly = true;
+            this.decodedValue.Size = new System.Drawing.Size(260, 187);
+            this.decodedValue.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(135, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "HResult Decoded:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "HResult:";
+            // 
+            // hresultInput
+            // 
+            this.hresultInput.Location = new System.Drawing.Point(13, 29);
+            this.hresultInput.Name = "hresultInput";
+            this.hresultInput.Size = new System.Drawing.Size(259, 20);
+            this.hresultInput.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 326);
+            this.Controls.Add(this.hresultInput);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.decodedValue);
+            this.Controls.Add(this.decodeButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "HResult Decoder";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button decodeButton;
+        private System.Windows.Forms.TextBox decodedValue;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox hresultInput;
+    }
+}
+
---
+++ HResultDecoder/Form1.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+// See page: http://msdn.microsoft.com/en-us/library/cc231198.aspx
+namespace HResultDecoder
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            uint hresult = 0x0;
+            try
+            {
+                hresult = Convert.ToUInt32(hresultInput.Text, 16);
+            }
+            catch (Exception)
+            {
+                decodedValue.Text = "That's not a valid HResult, fuck you.";
+                return;
+            }
+
+            bool severity = IsBitSet(hresult, 0);
+            bool reservedR = IsBitSet(hresult, 1);
+            bool customer = IsBitSet(hresult, 2);
+            bool ntstatus = IsBitSet(hresult, 3);
+            bool reservedX = IsBitSet(hresult, 4);
+            int facility = GetFacility(hresult);
+            int code = GetCode(hresult);
+
+            decodedValue.Text = string.Format(
+@"Code Value: {7}
+Facility Code: {5}
+Facility Name: {6}
+
+---- Header Bit Values ----
+    Severity (S): {0}
+    Reserved (R): {1}
+    Customer (C): {2}
+    NTStatus (N): {3}
+    Reserved (X): {4}",
+ severity ? "Failure" : "Success",
+ reservedR ? "Set" : "Not Set",
+ customer ? "Customer Code" : "Microsoft Code",
+ ntstatus ? "Set" : "Not Set",
+ reservedX ? "Set (Not Supposed To Happen)" : "Not Set",
+ facility,
+ GetFacilityName(facility),
+ code
+ );
+
+        }
+
+        private static bool IsBitSet(uint hresult, int bitIndex)
+        {
+            const uint zero = 0;
+            return (hresult & (zero | (uint)Math.Pow(2, bitIndex))) > 0;
+        }
+
+        private static int GetFacility(uint hresult)
+        {
+            return (int)((hresult << 5) >> 21);
+        }
+
+        private static int GetCode(uint hresult)
+        {
+            return (int)((hresult << 16) >> 16);
+        }
+
+        private static string GetFacilityName(int facility)
+        {
+            switch (facility)
+            {
+                case 1:
+                    return "RPC subsystem";
+                case 2:
+                    return "COM Dispatch";
+                case 3:
+                    return "OLE Storage";
+                case 4:
+                    return "COM/OLE Interface management";
+                case 7:
+                    return "Reserved for WIN32 internal crap";
+                case 8:
+                    return "Windows subsystem";
+                case 9:
+                    return "Security API layer";
+                case 10:
+                    return "Control mechanism";
+                case 11:
+                    return "Certificate or client server";
+                case 12:
+                    return "Wininet related";
+                case 13:
+                    return "Windows Media Server";
+                case 14:
+                    return "Microsoft Message Queue";
+                case 15:
+                    return "Setup API";
+                case 16:
+                    return "Smart-cars subsystem";
+                case 17:
+                    return "COM+";
+                case 18:
+                    return "Microsoft Agent";
+                case 19:
+                    return ".Net Common Language Runtime";
+                case 20:
+                    return "Audit collection service";
+                case 21:
+                    return "Direct Play";
+                case 22:
+                    return "Ubiquitous Memoryintrospection Service";
+                case 23:
+                    return "Side-by-side servicing";
+                case 24:
+                    return "Windows CE";
+                case 25:
+                    return "Http support";
+                case 26:
+                    return "Common Logging support";
+                case 31:
+                    return "User mode filter manager";
+                case 32:
+                    return "Background copy control";
+                case 33:
+                    return "Configuration service";
+                case 34:
+                    return "State Management service";
+                case 35:
+                    return "Microsoft Identity Server";
+                case 36:
+                    return "Windows Update";
+                case 37:
+                    return "Active Directory service";
+                case 38:
+                    return "Graphics drivers";
+                case 39:
+                    return "User Shell";
+                case 40:
+                    return "Trusted Platform Module service";
+                case 41:
+                    return "Trusted Platofrm Module applications";
+                case 48:
+                    return "Perfomance Logs and Alerts";
+                case 49:
+                    return "Full volume encryption";
+                case 50:
+                    return "Firewall Platform";
+                case 51:
+                    return "Windows Resource Manager";
+                case 52:
+                    return "Network Driver Interface";
+                case 53:
+                    return "Usermode Hypervisor components";
+                case 54:
+                    return "Configuration Managemeent Interface";
+                case 55:
+                    return "Usermode virtualization subsystem";
+                case 56:
+                    return "Usermode volume manager";
+                case 57:
+                    return "Boot Configuration Database";
+                case 58:
+                    return "Usermode virtual hard disk support";
+                case 60:
+                    return "System Diganostics";
+                case 61:
+                    return "Web Services";
+                case 80:
+                    return "A Windows Defender component";
+                case 81:
+                    return "Open Connectivity Service";
+                default:
+                    return "Unknown (not listed in MSDN documentation)";
+            }
+        }
+    }
+}
